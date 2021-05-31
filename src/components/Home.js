0e3c6b2b5b9abe6ed94b7b351ff3ea21ebced3f9
madara <wcamacho221@gmail.com>
2021-05-30 21:35:23 -0500

    Carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import styled from 'styled-components'
+import ImageSlider from './ImageSlider'
+import Viewer from './Viewer'
 
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+        <Fragment>
+            <Container>
+                <ImageSlider />
+                <Viewer/>
+            </Container>
+        </Fragment>
     )
 }
 
 export default Home
+
+
+//Componentes
+const Container = styled.main`
+    min-height:calc(100vh - 70px);
+    padding: 0 calc(3.5vw - 5px);
+    position: relative;
+    overflow-x: hidden;
+
+    &:before{
+        content:"";
+        background : url("/images/home-background.png") center center / cover no-repeat fixed;
+        position:absolute;
+        top:0;
+        bottom:0;
+        left:0;
+        right:0;
+        z-index:-1;
+    }
+`
---
+++ src/components/Viewer.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const Viewer = () => {
+    return (
+        <Fragment>
+            
+        </Fragment>
+    )
+}
+
+export default Viewer
